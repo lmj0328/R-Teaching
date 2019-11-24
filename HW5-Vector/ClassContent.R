1f6e824731494cbf47a658f022b35505c44e17c0
lmj0328 <lmj0328@uw.edu>
2019-11-23 21:26:41 -0800

    check merge

--- HW5-Vector/ClassContent.R
+++ HW5-Vector/ClassContent.R
@@ -36,9 +36,10 @@ print(Tim_data) # "Tianyi Li" "September 26th" "181"
 
 print(Tim_data[5]) # NA
 
+#### 7.3.2 Multiple Indices
 
 # Create a `colors` vector
-colors <- c("red", "green", "blue", "yellow", "purple")
+colors <- c("red", "green", "blue", "yellow", "purple") #vector
 
 # Vector of indices to extract
 indices <- c(1, 3, 4)
@@ -55,4 +56,68 @@ indices[colors]
 indices[c(1, 2)]
 
 indices[c(2, 5)]
+
+
+#### 7.3.3 Logical Indexing
+
+# Create a vector of shoe sizes
+shoe_sizes <- c(7, 6.5, 4, 11, 8)
+
+# Vector of elements to extract
+filter <- c(TRUE, FALSE, FALSE, TRUE, TRUE)
+
+# Extract every element in an index that is TRUE
+shoe_sizes[filter]  # [1]  7 11  8
+
+shoe_sizes + 5
+
+shoe_sizes * 2
+# colors * 2 #run error
+
+shoe_sizes == 6.5
+
+colors == "red" #vector contains logical
+colors[colors == "red"] #vector
+
+
+#### 7.3.4 Named Vectors and Character Indexing
+
+#directly assign
+param <- c(gamma=3, alpha=1.7, "c-2"=-1.33)
+
+subject <- c("Math", "English", "AP GOV", "Econ")
+
+grades <- c(70, 50, 80, 20)
+
+names(grades) <- subject
+
+grades[c("Math", "AP GOV")]
+
+
+#### 7.4 Modifying Vectors
+
+grades["Math"] <- 100
+grades[1] <- 0
+grades[c(1, 2)] <- c(20, 40)
+# grades[1, 2] <- c(20, 40)
+
+
+#### 7.5.1 Vectorized Operators
+
+# Create two vectors to combine
+v1 <- c(2, 1, 1, 1)
+v2 <- c(1, 2, 3, 4, 5, 6, 7, 8)
+
+# Create arithmetic combinations of the vectors
+v1 + v2  # returns 2, 3, 4, 5, 6
+v1 - v2  # returns 0, -1, -2, -3, -4
+v1 * v2  # returns 1, 2, 3, 4, 5
+v1 / v2  # returns 1, .5, .33, .25, .2
+
+# Add a vector to itself (why not?)
+v3 <- v2 + v2  # returns 2, 4, 6, 8, 10
+
+# Perform more advanced arithmetic!
+v4 <- (v1 + v2) / (v1 + v1)  # returns 1, 1.5, 2, 2.5, 3
+
 
